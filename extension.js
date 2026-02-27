4f3b61223602db97096ef49d16dac61231c01964
Rishank Sharma <rishanksharma04524@gmail.com>
2026-02-27 16:13:57 +0530

    feat: initialize VS Code extension with sound effect command
    - Added package.json to define extension metadata and dependencies.
    - Created extension test suite in extension.test.js for basic functionality.
    - Added vsc-extension-quickstart.md for setup and usage instructions.

---
+++ extension.js
@@ -0,0 +1,58 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const path = require('path');
+const { exec } = require('child_process');
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "ctrl-v-sound-effect" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	
+		// The code you place here will be executed every time your command is executed
+		
+        const soundPath = path.join(context.extensionPath, 'media/bruh-sound.wav');
+		console.log('Playing sound from path:', soundPath);
+
+	const disposable = vscode.workspace.onDidChangeTextDocument(async (event) => {
+
+    if (event.contentChanges.length === 0) return;
+
+    const change = event.contentChanges[0];
+
+    const clipboardText = await vscode.env.clipboard.readText();
+
+    // If inserted text matches clipboard → likely paste
+    if (change.text === clipboardText && change.text.length > 0) {
+        exec(`powershell -Command "$player = New-Object System.Media.SoundPlayer(\\"${soundPath}\\"); $player.PlaySync();"`);
+
+    }
+});
+
+		
+	
+
+	// Add disposable to context subscriptions
+	context.subscriptions.push(disposable);
+
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
